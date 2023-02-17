f353d797d3ae25698e0c253732053a345515b118
Emiliano Garcia DLR <emoliamolia@gmail.com>
2023-02-17 05:59:28 -0600

    actualizacion de examen

--- CDN.js
+++ CDN.js
@@ -18,10 +18,204 @@ import {
     setDoc,
     getDocs,
     doc,
-    getDoc,
-    updateDoc,
     deleteDoc,
 } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-firestore.js";
+
+function playgame() {
+    var config = {
+        type: Phaser.AUTO,
+        width: 860,
+        height: 1100,
+        physics: {
+            default: 'arcade',
+            arcade: {
+                gravity: { y: 600 },
+                debug: false
+            }
+        },
+        scene: {
+            preload: preload,
+            create: create,
+            update: update
+        }
+    };
+    
+    var player;
+    var stars;
+    var bombs;
+    var platforms;
+    var cursors;
+    var score = 0;
+    var gameOver = false;
+    var scoreText;
+    
+    var game = new Phaser.Game(config);
+    
+    function preload ()
+    {
+        this.load.image('sky', 'assets/fondog.png');
+        this.load.image('ground', 'assets/pisoa.png');
+        this.load.image('star', 'assets/emi-2.png');
+        this.load.image('bomb', 'assets/bomb.png');
+        this.load.spritesheet('dude', 'assets/mono.png', { frameWidth: 32, frameHeight: 48 });
+    }
+    
+    function create ()
+    {
+        //  A simple background for our game
+        this.add.image(400, 300, 'sky').setScale(.9);
+    
+        //  The platforms group contains the ground and the 2 ledges we can jump on
+        platforms = this.physics.add.staticGroup();
+    
+        //  Here we create the ground.
+        //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
+        platforms.create(100, 700, 'ground').setScale(50, .5).refreshBody();
+    
+        //  Now let's create some ledges
+        platforms.create(700, 650, 'ground');
+        platforms.create(260, 550, 'ground');
+        platforms.create(500, 600, 'ground');
+        platforms.create(60, 500, 'ground');
+        platforms.create(260, 450, 'ground');
+        platforms.create(550, 700, 'ground');
+        platforms.create(670, 330, 'ground');
+        platforms.create(400, 100, 'ground');
+        platforms.create(500, 400, 'ground');
+        
+
+    
+        // The player and its settings
+        player = this.physics.add.sprite(100, 450, 'dude');
+    
+        //  Player physics properties. Give the little guy a slight bounce.
+        player.setBounce(0.2);
+        player.setCollideWorldBounds(true);
+    
+        //  Our player animations, turning, walking left and walking right.
+        this.anims.create({
+            key: 'left',
+            frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+            frameRate: 10,
+            repeat: -1
+        });
+    
+        this.anims.create({
+            key: 'turn',
+            frames: [ { key: 'dude', frame: 4 } ],
+            frameRate: 20
+        });
+    
+        this.anims.create({
+            key: 'right',
+            frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+            frameRate: 10,
+            repeat: -1
+        });
+    
+        //  Input Events
+        cursors = this.input.keyboard.createCursorKeys();
+    
+        //  Some stars to collect, 12 in total, evenly spaced 70 pixels apart along the x axis
+        stars = this.physics.add.group({
+            key: 'star',
+            setXY: { x: 100, y: 50, stepX: 1000 }
+        });
+    
+        stars.children.iterate(function (child) {
+    
+            //  Give each star a slightly different bounce
+            child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+    
+        });
+    
+        bombs = this.physics.add.group();
+    
+        //  The score
+        scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
+    
+        //  Collide the player and the stars with the platforms
+        this.physics.add.collider(player, platforms);
+        this.physics.add.collider(stars, platforms);
+        this.physics.add.collider(bombs, platforms);
+    
+        //  Checks to see if the player overlaps with any of the stars, if he does call the collectStar function
+        this.physics.add.overlap(player, stars, collectStar, null, this);
+    
+        this.physics.add.collider(player, bombs, hitBomb, null, this);
+    }
+    
+    function update ()
+    {
+        if (gameOver)
+        {
+            return;
+        }
+    
+        if (cursors.left.isDown)
+        {
+            player.setVelocityX(-160);
+    
+            player.anims.play('left', true);
+        }
+        else if (cursors.right.isDown)
+        {
+            player.setVelocityX(160);
+    
+            player.anims.play('right', true);
+        }
+        else
+        {
+            player.setVelocityX(0);
+    
+            player.anims.play('turn');
+        }
+    
+        if (cursors.up.isDown && player.body.touching.down)
+        {
+            player.setVelocityY(-330);
+        }
+    }
+    
+    function collectStar (player, star)
+    {
+        star.disableBody(true, true);
+    
+        //  Add and update the score
+        score += 10;
+        scoreText.setText('Score: ' + score);
+    
+        if (stars.countActive(true) === 0)
+        {
+            //  A new batch of stars to collect
+            stars.children.iterate(function (child) {
+    
+                child.enableBody(true, child.x, 0, true, true);
+    
+            });
+    
+            var x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
+    
+            var bomb = bombs.create(x, 16, 'bomb');
+            bomb.setBounce(1);
+            bomb.setCollideWorldBounds(true);
+            bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+            bomb.allowGravity = false;
+    
+        }
+    }
+    
+    function hitBomb (player, bomb)
+    {
+        this.physics.pause();
+    
+        player.setTint(0xff0000);
+    
+        player.anims.play('turn');
+    
+        gameOver = true;
+    }
+}
 
 const firebaseConfig = {
     apiKey: "AIzaSyBcKoDCCMIBZY8KetZ6jQNTGbtiRfNWKh4",
@@ -47,6 +241,8 @@ const db = getFirestore(app); // Initialize Cloud Firestore and get a reference 
 const log = document.getElementById("log");
 const email = document.getElementById("email");
 const pass = document.getElementById("pass");
+const registros = document.getElementById("registros");
+
 //reguistro con apps
 const gog = document.getElementById("google");
 const face = document.getElementById("facebook");
@@ -74,10 +270,15 @@ const inputActedad = document.getElementById("edad-ac");
 //delete borrar--------
 const borrar = document.getElementById("borrar");
 
-//divs nesesarios
+//nesesarios
 const header = document.getElementById("header");
 const div = document.getElementById("inicio");
 const divOcultar = document.getElementById("ocultar");
+const cuentasocultas = document.getElementById("cuentas");
+const play = document.getElementById("play");
+const a = document.getElementById("1");
+const regresar = document.getElementById("back");
+const juego = document.getElementById("jugar");
 
 //-----mapa------
 const showmap = document.getElementById("mostrarmap");
@@ -88,7 +289,6 @@ const cerrarmapa = document.getElementById("cerrarmapa");
 // const borrar = document.getElementById("borrar");
 
 //crear un usuario nuevo
-
 crear.addEventListener("click", function () {
     createUserWithEmailAndPassword(auth, email.value, pass.value)
         .then((userCredential) => {
@@ -119,6 +319,13 @@ log.addEventListener("click", function () {
             const errorMessage = error.message;
             alert(errorCode + " + " + errorMessage);
         });
+});
+
+registros.addEventListener('click', function ( ) {
+    cuentasocultas.classList.remove("hide");
+    div.classList.add("hide");
+    
+
 });
 
 cerrar.addEventListener("click", function () {
@@ -247,14 +454,15 @@ onAuthStateChanged(auth, (user) => {
     }
 });
 
+
+// --------------------CRUD------------------------
 guardar.addEventListener("click", async () => {
     try {
         await setDoc(doc(db, "users", nombre.value), {
             Nombre: nombre.value,
-            Apellidos: apellido.value,
-            Edad: edad.value,
+            Region: region.value,
         });
-        alert(`Documento ${nombre.value} creado!`);
+        alert(`Tu Nickname ${nombre.value} se ha creado correctamente!`);
     } catch (error) {
         alert("No has completado todos los datos correspondientes");
         console.error("Error adding document: ", e);
@@ -264,9 +472,9 @@ guardar.addEventListener("click", async () => {
 leer.addEventListener("click", async () => {
     tabla.innerHTML = `<tr>
         <td> |---Id---| </td>
-        <td> |---Nombre---| </td>
-        <td> |---Apellido---| </td>
-        <td> |---Edad---|   </td>
+        <td> |---Nickname---| </td>
+        <td> |---Region---| </td>
+        <td> |---Score---| </td>
     </tr>`;
 
     const querySnapshot = await getDocs(collection(db, "users"));
@@ -275,45 +483,25 @@ leer.addEventListener("click", async () => {
         tabla.innerHTML += `<tr>
             <td>${doc.id}</td>
             <td>${doc.data().Nombre}</td>
-            <td>${doc.data().Apellidos}</td>
-            <td>${doc.data().Edad}</td>
+            <td>${doc.data().Region}</td>
         </tr>`;
     });
 });
 
-BuscarAct.addEventListener("click", async () => {
-    const docRef = doc(db, "users", inputActid.value);
-    const docSnap = await getDoc(docRef);
-
-    if (docSnap.exists()) {
-        inputActname.value = docSnap.data().Nombre;
-        inputActlast.value = docSnap.data().Apellidos;
-        inputActedad.value = docSnap.data().Edad;
-        console.log("Document data:", docSnap.data());
-    } else {
-        // doc.data() will be undefined in this case
-        console.log("No such document!");
-        alert(
-            "No se encontraron los datos con este ID, O Este ID es incorrecto"
-        );
-    }
+play.addEventListener('click', function(){
+    a.classList.remove("hide");
+    divOcultar.classList.add("hide")
 });
 
-actualizar.addEventListener("click", async () => {
-    const elementRef = doc(db, "users", inputActid.value);
-
-    await updateDoc(elementRef, {
-        Nombre: inputActname.value,
-        Apellidos: inputActlast.value,
-        Edad: inputActedad.value,
-    });
-    alert("se actualizaron los datos de " + inputActid);
+regresar.addEventListener('click', function(){
+    a.classList.add("hide");
+    divOcultar.classList.remove("hide")
 });
 
 borrar.addEventListener("click", async () => {
-    await deleteDoc(doc(db, "users", inputActid.value));
+    await deleteDoc(doc(db, "users", nombre.value));
 
-    alert("se ha borrado todo el usuario de " + inputActid);
+    alert("Se ha borrado el usuario "  +  nombre.value  + ' junto con sus datos');
 });
 
 //-----------------mapbox-----------------
@@ -341,10 +529,25 @@ showmap.addEventListener("click", function () {
     mapa.classList.remove("hide");
     showmap.classList.add("hide");
     cerrarmapa.classList.remove("hide");
+    a.classList.add("hide");
+    
 });
 
 cerrarmapa.addEventListener("click", function () {
     mapa.classList.add("hide");
     showmap.classList.remove("hide");
     cerrarmapa.classList.add("hide");
+    a.classList.remove("hide");
 });
+
+
+// -----------------------phaser------------
+juego.addEventListener("click", function () {
+    a.classList.add("hide");
+    playgame ()
+});
+
+
+
+
+
